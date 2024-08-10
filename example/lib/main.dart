b33527393e67337f81e6519c24f304c29395c3e1
Richard Yang <richard.yang.cn@hotmail.com>
2024-08-10 18:39:38 +0800

    fix issue in release build

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -71,7 +71,9 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
 
     mapProvider.loadProviders().then((val) {
-      setState(() {});
+      setState(() {
+        currentMapProviderName = "OpenStreetMap.CH";
+      });
     });
 
     polyline = Polyline(
@@ -237,9 +239,8 @@ class _MyAppState extends State<MyApp> {
                         child: Text(
                           e.name,
                           style: TextStyle(
-                              color: e.auth.isNotEmpty
-                                  ? Colors.red
-                                  : Colors.green,
+                              color:
+                                  e.auth.isNotEmpty ? Colors.red : Colors.green,
                               backgroundColor: e.name == currentMapProviderName
                                   ? Colors.blueGrey
                                   : Colors.transparent),
@@ -254,7 +255,7 @@ class _MyAppState extends State<MyApp> {
                   });
                 },
               ),
-              if (currentMapProviderName != null)
+              if (currentMapProviderName != null && curProv != null)
                 Expanded(
                   child: FlutterMap(
                     options: MapOptions(
